ed1756064748702e862cd7dd2f222bf1b153cd48
Sultan Apsalyamov <sapsalamov@gmail.com>
2020-09-29 21:08:42 +0600

    Add animation On mouseover, mouseout event adds the highlight text animation.

--- week2/codecademy_lesson/climate_change_disease_visualisation/main.js
+++ week2/codecademy_lesson/climate_change_disease_visualisation/main.js
@@ -16,6 +16,40 @@ let climate_daly_data = [
 
 
 let toggleClass = (i,toggle) => {
-   d3.select("#viz div:nth-child("+ i +")").classed("highlightBar",toggle);
-   d3.select("#legend li:nth-child("+ i +")").classed("highlightText",toggle);
+   d3.select("#viz div:nth-child("+ i +")")
+   .classed("highlightBar",toggle);
+
+   d3.select("#legend li:nth-child("+ i +")")
+   .classed("highlightText",toggle);
 };
+
+divSelection = d3.select('#viz')
+  .selectAll('div')
+    .data(climate_daly_data)
+    .enter()
+  .append('div')
+    .attr('class', 'bar')
+    .style('width', function(d) {
+        return d.deaths * 8 + "px";
+    })
+    .on("mouseover", function(d, i) {
+      toggleClass(i+1, true);
+    })
+    .on("mouseout", function(d, i) {
+      toggleClass(i+1, false);
+    });
+
+listSelection = d3.select('#legend')
+  .selectAll('li')
+    .data(climate_daly_data)
+    .enter()
+  .append('li')
+    .text(function(d) {
+      return d.region + ": " + d.deaths + " deaths";
+    })
+    .on("mouseover", function(d, i) {
+      toggleClass(i+1, true);
+    })
+    .on("mouseout", function(d, i) {
+      toggleClass(i+1, false);
+    });
