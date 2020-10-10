75008ee6d487a1729a0e585a14a134346c181b66
Sultan Apsalyamov <sapsalamov@gmail.com>
2020-10-10 16:40:33 +0600

    Change the week2 chart

--- week2/app.js
+++ week2/app.js
@@ -8,26 +8,56 @@ const DATA = [
     {id:4, value:1, name:"Ukr"},
 ]
 
+
+const listItems = d3.select('ul')
+.selectAll('li')
+.data(DATA, (data)=>data.name)
+.enter()
+.append('li');
+
+listItems.append('span')
+.text((data)=>data.name);
+
+
+const xScale = d3.scaleBand()
+.domain(DATA.map( (dp) => dp.name )).rangeRound([0,250]).padding(0.1);
+
+const yScale = d3.scaleLinear().domain([0,12]).rangeRound([0,200]);
+
+
 const container = d3.select("svg")
-.classed('container', true)
-/*.style("border", "1px solid #CC1111")*/;
-
-xScale = d3.scaleBand()
-.domain(DATA.map( (db) => db.name)).rangeRound([0,250]).padding(0.1);
-
-const yScale = d3.scaleLinear().domain([0,12]).rangeRound([0, 200])
+.append('g')
+.call(d3.axisBottom(xScale))
+.attr('color','#DD1111')
 
 const bars = container
 .selectAll(".bar")
 .data(DATA)
 .enter()
 .append('rect')
-.classed('bar', true)
-.attr('width', xScale.bandwidth())
-.attr('height', data=>yScale(data.value))
+.classed('bar',true)
+.attr('width',xScale.bandwidth())
+.attr('height',data=>yScale(data.value))
 .attr('x',data=>xScale(data.name))
-.attr('y',data=>200-yScale(data.value));
+.attr('y', data=>200-yScale(data.value));
+// .text(data=>data.value);
 
+listItems
+.append('input')
+.attr('type','checkbox')
+.attr('checked',true)
+.attr('id',(data)=>data.id)
+.on('change', (evnt)=>{
+  console.log(evnt.target.id);
+  cid = parseInt(evnt.target.id);
+  var filtered = DATA.filter((el)=>el.id!==cid);
+  console.log(filtered);
+  d3.select('.bar')
+  .data(filtered, data=>data.name)
+  .exit()
+  .remove();
+
+});
 
 const Month = d3.scaleBand()
 .domain(['Jan', 'Feb', 'Mar', 'Apr', 'May'])
