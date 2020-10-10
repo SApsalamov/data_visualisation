5da681f942eb4fe75d414ef809b9ad65765e271f
Sultan Apsalyamov <sapsalamov@gmail.com>
2020-10-10 19:44:01 +0600

    HW2 Remove add bars

--- week2/app.js
+++ week2/app.js
@@ -2,10 +2,10 @@ console.log(d3)
 console.log("Hello world!")
 
 const DATA = [
-    {id:1, value:10, name:"Kaz"},
-    {id:2, value:5, name:"Rus"},
-    {id:3, value:3, name:"Kyr"},
-    {id:4, value:1, name:"Ukr"},
+    {id:1, value:10, name:"Kaz", check:1},
+    {id:2, value:5, name:"Rus", check:1},
+    {id:3, value:3, name:"Kyr", check:1},
+    {id:4, value:1, name:"Ukr", check:1},
 ]
 
 
@@ -20,15 +20,19 @@ listItems.append('span')
 
 
 const xScale = d3.scaleBand()
-.domain(DATA.map( (dp) => dp.name )).rangeRound([0,250]).padding(0.1);
+  .domain(DATA.map( (dp) => dp.name ))
+  .rangeRound([0,250])
+  .padding(0.1);
 
-const yScale = d3.scaleLinear().domain([0,12]).rangeRound([0,200]);
+const yScale = d3.scaleLinear()
+  .domain([0,12])
+  .rangeRound([0,200]);
 
 
 const container = d3.select("svg")
 .append('g')
 .call(d3.axisBottom(xScale))
-.attr('color','#DD1111')
+.attr('color','#DD1111');
 
 const bars = container
 .selectAll(".bar")
@@ -39,23 +43,63 @@ const bars = container
 .attr('width',xScale.bandwidth())
 .attr('height',data=>yScale(data.value))
 .attr('x',data=>xScale(data.name))
-.attr('y', data=>200-yScale(data.value));
+.attr('y', data=>200-yScale(data.value))
+.attr('id', (data)=>"bar"+ data.id);
 // .text(data=>data.value);
 
 listItems
 .append('input')
 .attr('type','checkbox')
 .attr('checked',true)
-.attr('id',(data)=>data.id)
+.attr('id',(data)=>"inp" + data.id)
 .on('change', (evnt)=>{
   console.log(evnt.target.id);
-  cid = parseInt(evnt.target.id);
-  var filtered = DATA.filter((el)=>el.id!==cid);
-  console.log(filtered);
-  d3.select('.bar')
-  .data(filtered, data=>data.name)
-  .exit()
-  .remove();
+
+  var re = new RegExp("[0-4]");
+
+  m = re.exec(evnt.target.id);
+  console.log('m ' + m );
+  cid = parseInt(m);
+  console.log(DATA[cid-1]['id']);
+  console.log(DATA.length);
+  for (var i = 0; i < DATA.length; i++) {
+    if ((DATA[i]['id']) == cid) {
+      if (DATA[i]['check'] == 1) {
+        DATA[i]['check'] = 0;
+        console.log(DATA[cid-1]['id']);
+        var filtered = DATA.filter((el)=>el.check!==0);
+        console.log('console.log(filtered);');
+        console.log(filtered);
+        console.log('id ' + '#bar'+cid);
+
+        container.selectAll('.bar')
+        .data(filtered, data=>data.id)
+        .exit()
+        .remove();
+      } else {
+        DATA[i]['check'] = 1;
+
+        var filtered_add = DATA.filter((el)=>el.check==1);
+        console.log('console.log(filtered_add);');
+        console.log(console.log(filtered_add));
+        var appending = container
+        .selectAll('.bar')
+        .data(filtered_add, data=>data.name)
+        .enter()
+        .append('rect')
+        .classed('bar',true)
+        .attr('width',xScale.bandwidth())
+        .attr('height',data=>yScale(data.value))
+        .attr('x',data=>xScale(data.name))
+        .attr('y', data=>200-yScale(data.value))
+        .attr('id', (data)=>"bar"+ data.id);
+      }
+    }
+  }
+
+
+
+
 
 });
 
